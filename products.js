3df3bef225b61db04eef2f373c0c1ec7b517042d
Chhaya Bobade <91379325+Chhaya-Bobade@users.noreply.github.com>
2023-02-10 17:36:07 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,50 @@
+let slide_left_btn = document.getElementById('slides-left')
+let slide_right_btn = document.getElementById('slides-right')
+
+let left_slider = document.getElementById('left_sliders')
+let right_slider = document.getElementById('right_sliders')
+
+let num_of_slides = right_slider.querySelectorAll('.slides').length
+let current_slide = 1;
+
+slide_left_btn.addEventListener('click', function(e) {
+  slide_left()
+})
+
+slide_right_btn.addEventListener('click', function(e) {
+  slide_right()
+})
+
+function slide_left(){
+
+  if (current_slide === 1) {
+    current_slide = 3
+  } else {
+    current_slide--
+  }
+
+  document.querySelectorAll('.slides').forEach(function(slide) {
+    slide.style.transform = `translateX(-${(current_slide - 1) * 100}%)`
+  })
+}
+
+function slide_right(){
+
+  if (current_slide === num_of_slides) {
+    current_slide = 1
+  } else {
+    current_slide++
+  }
+
+  document.querySelectorAll('.slides').forEach(function(slide) {
+    slide.style.transform = `translateX(-${(current_slide - 1) * 100}%)`
+  })
+}
+
+let auto = setInterval(function(){
+  slide_right()
+}, 5000);
+
+
+// sidebar start
+
